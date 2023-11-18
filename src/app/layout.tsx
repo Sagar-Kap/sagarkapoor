91e7ff909ca348516c3a7a8faf61c81724102745
Sagar Kapoor <69609200+Sagar-Kap@users.noreply.github.com>
2023-11-18 08:59:28 +0200

    feat: initiated layout of the homepage with Nav & footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { ubuntu } from "../styles/fonts";
 import "@styles/globals.css";
+import Navbar from "@components/layout/navbar";
+import Footer from "@components/layout/footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={ubuntu.className}>{children}</body>
+      <body className={ubuntu.className}>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
