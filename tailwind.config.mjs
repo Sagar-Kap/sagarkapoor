f572969e6765bab2b44cddd0261c8047fa28c9d8
Sagar Kapoor <69609200+Sagar-Kap@users.noreply.github.com>
2023-11-27 21:59:17 +0200

    feat: initiated NavBar component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          1: "#12F7D6",
+          2: "#98FAEC",
+        },
+        grey: "#98FAEC",
+        html: "#E54F26",
+        css: "#0C73B8",
+        js: "#E7A020",
+        react: "#28A9E0",
+        bg: {
+          1: "#292F36",
+          2: "#1A1E23",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
