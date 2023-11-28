dfe2601f46106c95206854510b15ce28d92da47c
Sagar Kapoor <69609200+Sagar-Kap@users.noreply.github.com>
2023-11-28 10:35:27 +0200

    style: added custom font families to tailwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        mono: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
+        sans: ["Ubuntu", ...defaultTheme.fontFamily.sans],
+      },
+
       colors: {
         brand: {
           1: "#12F7D6",
