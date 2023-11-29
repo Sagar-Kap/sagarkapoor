81ebb124b5173e849ce9cdef31552314f303a152
Sagar Kapoor <69609200+Sagar-Kap@users.noreply.github.com>
2023-11-29 13:53:28 +0200

    refactor: temporary code added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import Icons from "unplugin-icons/vite";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact()],
+  vite: {
+    plugins: [Icons({
+      compiler: "astro",
+      autoInstall: true
+    })]
+  }
 });
---
+++ src/components/layout/NavBar/MenuButton.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "preact/hooks";
+
+const MenuButton = () => {
+  const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
+  return <div>MenuButton</div>;
+};
+
+export default MenuButton;
