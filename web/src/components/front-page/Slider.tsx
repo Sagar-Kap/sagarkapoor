dbdb8ef9cfae0e77fb8b2c63c1d7149781f1a390
Sagar-Kap <69609200+Sagar-Kap@users.noreply.github.com>
2024-02-23 08:21:39 +0200

    chore: added assets for work section on index page

--- web/src/components/front-page/Slider.tsx
+++ web/src/components/front-page/Slider.tsx
@@ -1,5 +1,25 @@
+import Phone from "../../assets/phone.svg";
+import Monitor from "../../assets/monitor.svg";
+import Pointer from "~icons/lucide/mouse-pointer";
+
 const Slider = () => {
-  return <div class="text-white">Slider</div>;
+  return (
+    <section class="border m-3 col-span-2 sm:col-span-6 sm:col-start-2">
+      <p>&lt;</p>
+      <p>
+        Visit Website <Pointer />
+      </p>
+      <img
+        src={Phone.src}
+        alt={"Emulation of the project on a phone's screen."}
+      />
+      <img
+        src={Monitor.src}
+        alt={"Emulation of the project on a monitor's screen."}
+      />
+      <p>&gt;</p>
+    </section>
+  );
 };
 
 export default Slider;
