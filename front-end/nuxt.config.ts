c8116b80e2908deddd597ccdf1e248758d0717d7
Indranil <indranilchakraborty4sc@gmail.com>
2024-07-21 11:22:27 +0000

    feat(ui): setup sass for nuxt

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint"],
+  css: [
+    // SCSS file in the project
+    "~/assets/css/main.scss",
+  ],
 });
--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>The Home Page</div>
+  <div class="container">
+    <h1>Welcome to Nuxt with SASS</h1>
+    <p>This is a demo page to test SASS integration in a Nuxt.js project.</p>
+  </div>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  head() {
+    return {
+      title: "Nuxt SASS Demo",
+    };
+  },
+};
+</script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "../assets/css/main.scss" .container {
+  background-color: lighten($primary-color, 20%);
+  padding: 20px;
+  border-radius: 10px;
+
+  p {
+    font-size: 1.2rem;
+    text-align: center;
+  }
+}
+</style>
