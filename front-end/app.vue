dafade30781b16666c553ca77b5b831a8c7e6a60
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2024-08-04 13:35:36 +0530

    chore: add SASS support for the project (#173)
    * chore: add SASS support for the project
    
    * chore: refactor the SASS code to follow a standard approach

--- front-end/app.vue
+++ front-end/app.vue
@@ -3,3 +3,9 @@
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+body {
+  background-color: $BG1;
+}
+</style>
--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -3,4 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/styles/main.scss" as *;',
+        },
+      },
+    },
+  },
 });
