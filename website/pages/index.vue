bddfbe009cc6c79c61630536a5a69a133410a835
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-01-08 16:32:53 +0530

    chore: create the header for the index page (fixes #175)

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>The Home Page</div>
+  <div class="container">
+    <header>
+      <div class="tags">&lt;h1&gt;</div>
+      <p>Hey I am <span class="highlight">Sagar</span>, Full Stack Developer</p>
+      <div class="tags">&lt;/h1&gt;</div>
+    </header>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -12,3 +18,42 @@ useSeoMeta({
   twitterCard: "summary_large_image",
 });
 </script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 2rem;
+
+  header {
+    color: #ffffff;
+    font-style: Ubuntu;
+    font-weight: 500;
+    display: flex;
+    flex-direction: column;
+
+    .tags {
+      color: #2ad881;
+      font-size: 0.75rem;
+      line-height: 0.85rem;
+    }
+
+    p {
+      font-size: 2.5rem;
+      line-height: 2.875rem;
+
+      .highlight {
+        color: #2ad881;
+      }
+    }
+  }
+
+  // iPad Mini
+  @media screen and (min-width: 768px) {
+    padding: 4rem;
+  }
+
+  // Desktop
+  @media screen and (min-width: 1440px) {
+    padding: 9rem;
+  }
+}
+</style>
