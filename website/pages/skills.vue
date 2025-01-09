c59e78633ffa17fd0eda32a1c84f4d9f6b2bd437
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-01-09 16:37:49 +0530

    chore: implement the "skills" page (fixes #177)

--- website/pages/skills.vue
+++ website/pages/skills.vue
@@ -1,19 +1,31 @@
 <template>
   <main class="container">
     <div class="header">Skills</div>
-    <!-- TODO: Add a vertical line -->
-    <h2>Development</h2>
+
+    <div class="tagline">
+      <div class="vertical-line"></div>
+      <h2>Development</h2>
+    </div>
+
     <div class="grid">
-      <div>Icons</div>
-      <div>Icons</div>
-      <div>Icons</div>
-      <div>Icons</div>
-      <div>Icons</div>
-      <div>Icons</div>
-      <div>Icons</div>
-      <div>Icons</div>
-      <div>Icons</div>
-      <div>Icons</div>
+      <img src="/logos/cpp.svg" alt="logo of C++" />
+      <img src="/logos/css.svg" alt="logo of CSS" />
+      <img src="/logos/docker.svg" alt="logo of Docker++" />
+      <img src="/logos/graphql.svg" alt="logo of GraphQL" />
+      <img src="/logos/html.svg" alt="logo of HTML" />
+      <img src="/logos/javascript.svg" alt="logo of JavaScript" />
+      <img src="/logos/mongodb.svg" alt="logo of MongoDB" />
+      <img src="/logos/mysql.svg" alt="logo of MySQL" />
+      <img src="/logos/nodejs.svg" alt="logo of Node.js" />
+      <img src="/logos/postgresql.svg" alt="logo of PostgreSQL" />
+      <img src="/logos/python.svg" alt="logo of Python" />
+      <img src="/logos/react.svg" alt="logo of React" />
+      <img src="/logos/sass.svg" alt="logo of SASS" />
+      <img src="/logos/tailwindcss.svg" alt="logo of TailwindCSS" />
+      <img src="/logos/terraform.svg" alt="logo of Terraform" />
+      <img src="/logos/typescript.svg" alt="logo of TypeScript" />
+      <img src="/logos/unreal-engine.svg" alt="logo of Unreal Engine" />
+      <img src="/logos/vue.svg" alt="logo of Vue.js" />
     </div>
   </main>
 </template>
@@ -36,8 +48,8 @@ useSeoMeta({
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 4rem;
   padding: 1rem;
+  margin: 3rem auto;
 
   .header {
     font-family: Ubuntu;
@@ -56,6 +68,43 @@ useSeoMeta({
       rgba($color: #2ad881, $alpha: 0.06) 0.6%,
       rgba($color: #2ad881, $alpha: 0.25) 99.4%
     );
+  }
+
+  .tagline {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 4rem auto;
+
+    .vertical-line {
+      width: 2px;
+      height: 100px;
+      background-color: white;
+    }
+  }
+
+  .grid {
+    display: grid;
+    gap: 3rem;
+    grid-template-columns: repeat(2, 1fr);
+    align-items: center;
+    justify-items: center;
+  }
+
+  // iPad Mini
+  @media screen and (min-width: 768px) {
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+
+  // Desktop
+  @media screen and (min-width: 1440px) {
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(8, 1fr);
+    }
   }
 }
 </style>
